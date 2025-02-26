835f5d04f583cb8018c5dbb01a805f4ccebfb866
Morkva22 <marcusmay0202@gmail.com>
2025-02-26 22:04:59 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
+#include "UI/UI/UI.h"
+#include "Domain/Cafe/Cafe.h"
+#include "Domain/Menu/Menu.h"
+#include "Data/Data.h"
+#include "UI/Logger/Logger.h"
 
-int main(int argc, char* argv[])
-{
-    
+int main() {
+    Menu menu;
+    Data data;
+    Cafe cafe(&menu, &data);
+    Logger logger;
+    UI ui(&cafe, &logger);
+    ui.run();
     return 0;
 }
